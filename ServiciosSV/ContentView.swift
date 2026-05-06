3852dcbea5e8af57c24ecc6a2e0c7706b0b72a49
saraascencio <105680448+saraascencio@users.noreply.github.com>
2026-05-05 20:49:29 -0700

    Initial Commit

---
+++ ServiciosSV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ServiciosSV
+//
+//  Created by Sara Ascencio on 5/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ServiciosSV/ServiciosSVApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiciosSVApp.swift
+//  ServiciosSV
+//
+//  Created by Sara Ascencio on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ServiciosSVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
